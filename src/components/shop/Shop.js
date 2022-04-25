c21cc95abfb9e1db4f0886322155d989ab2b4281
md-sarower <sarower9415@gmail.com>
2022-04-25 09:50:44 +0600

    component and hook file added

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,44 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import useProducts from '../../hooks/useProduct';
 import { addToDb, getStoredCard } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 import Products from '../products/Products';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, []);
-
-    useEffect( () => {
         const storedCart = getStoredCard();
-        const saveCart = []; 
-        for(const id in storedCart) {
+        const saveCart = [];
+        for (const id in storedCart) {
             const addedProduct = products.find(product => product.id === id);
-            if(addedProduct){
+            if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
                 saveCart.push(addedProduct);
             }
         }
-        setCart(saveCart); 
+        setCart(saveCart);
     }, [products])
 
     const handleAddToCart = (selectedProduct) => {
         console.log(selectedProduct);
         let newCart = [];
         const exists = cart.find(product => product.id === selectedProduct.id);
-        if(!exists){
+        if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
-        else{
+        else {
             const rest = cart.filter(product => product.id !== selectedProduct.id);
-            exists.quantity = exists.quantity + 1; 
+            exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
         console.log(newCart);
