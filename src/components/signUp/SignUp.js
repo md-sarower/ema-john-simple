fd9b8176aea530e5262b1ecff9d8ba736d63bcf1
md-sarower <sarower9415@gmail.com>
2022-05-10 17:44:28 +0600

    Login form done with signInWithEmailAndPassword from office pc

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -16,12 +20,23 @@ const SignUp = () => {
     const handleConfirmPasswordBlur = event => {
         setConfirmPassword(event.target.value);
     }
+
+    if (user) {
+        navigate('/shop');
+    }
+
     const handleCreateUser = event => {
         event.preventDefault();
         if (password !== confirmPassword) {
             setError('Confirm password dose not matching with password')
             return;
         }
+        if (password.length < 6) {
+            setError('Password must be 6 characters or longer');
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password);
     }
 
     return (
